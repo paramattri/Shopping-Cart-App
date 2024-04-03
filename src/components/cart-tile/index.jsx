f1673d679fd8fef5e68bd817e2cf9c701da45b77
Param Attri <attri.param@gmail.com>
2024-04-03 17:10:03 +0530

    creates redux store to implement the cart functionality

---
+++ src/components/cart-tile/index.jsx
@@ -0,0 +1,38 @@
+import { useDispatch } from "react-redux";
+import { removeFromCart } from "../../store/slices/cartSlice";
+
+const CartTile = ({ cartItem }) => {
+  const dispatch = useDispatch();
+
+  const handleRemoveFromCart = () => {
+    dispatch(removeFromCart(cartItem.id));
+  };
+
+  return (
+    <div>
+      <div className="flex items-center p-5 justify-center bg-red-500 mt-5 mb-2 rounded-xl">
+        <div className="flex p-3">
+          <img
+            className="h-28 rounded-lg"
+            src={cartItem.image}
+            alt={cartItem.title}
+          />
+          <div className="ml-10 self-start space-y-5">
+            <h1 className="text-xl text-white font-bold">{cartItem.title}</h1>
+            <p className="text-white font-extrabold">{cartItem.price}</p>
+          </div>
+        </div>
+        <div>
+          <button
+            onClick={handleRemoveFromCart}
+            className="bg-red-950 text-white border-2 rounded-lg font-bold p-4"
+          >
+            Remove from cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartTile;
